1674761057134c5d1cc1503f135619f095edde2a
Yi leng Yao <yilengyao@gmail.com>
2024-05-03 13:05:49 -0700

    add java solution to container with most water

---
+++ Leetcode/Medium/container-with-most-water/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length - 1;
+        
+        int maxWater = 0;
+        while (start < end) {
+            maxWater = Math.max(maxWater, Math.min(height[start], height[end]) * (end - start));
+
+            if (height[start] < height[end]) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return maxWater;
+    }
+}
